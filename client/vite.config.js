62a0c861d2a832c35b9208dca81bf7ccb5a749bd
RadheyPrajapati <radheym2006@gmail.com>
2026-07-15 15:27:29 +0530

    Add allowedHosts to Vite preview configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['chatpdf-4-64uw.onrender.com']
+  }
 })
